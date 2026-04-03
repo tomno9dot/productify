0c0803d820507428f27983d639ff61f1ad313499
Bill Tomno <billtomno9@gmail.com>
2026-04-03 16:29:47 +0300

    initial commit

---
+++ backend/src/index.ts
@@ -0,0 +1,19 @@
+import express from 'express'
+import {ENV} from './config/env'
+import { clerkMiddleware } from '@clerk/express'
+import cors from 'cors'
+
+const app = express()
+
+app.use(clerkMiddleware())
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+app.use(cors({
+    origin: process.env.FRONTEND_URL
+}))
+
+app.get("/" , (req, res) => {
+    res.send("Server running")
+})
+
+app.listen(ENV.PORT, () => console.log("Server running on port:", ENV.PORT))
