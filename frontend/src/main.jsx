cbaa4c6dbc44e26f5df6850960f9c1390aa9a07c
Bill Tomno <billtomno9@gmail.com>
2026-04-06 22:02:09 +0300

    Refactor frontend structure: remove unused assets and styles, implement routing with new pages, and add a responsive Navbar with theme selection

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,21 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { ClerkProvider } from '@clerk/react'
+import {BrowserRouter} from 'react-router'
+
+const PUBLISHABLE_KEY= import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+     <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+      <BrowserRouter>
+      <App />
+      
+      </BrowserRouter>
+      
+
+     </ClerkProvider>
+    
   </StrictMode>,
 )
